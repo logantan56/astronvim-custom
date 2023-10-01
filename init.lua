23a84c1c2fe9f2b03e2053f84996576e13e1c8f9
Logan Tan <logantan56@gmail.com>
2023-10-01 19:40:33 -0400

    default to NeoSolarized theme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin",
+  colorscheme = "NeoSolarized",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
